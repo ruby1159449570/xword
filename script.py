341d3d7e9fcd6d50ccede2a8988d7068b5627ff7
Franxosorus Rex <franxg@gmail.com>
2020-09-06 23:08:59 +1000

    /Users/moonpie/xword/.git/COMMIT_EDITMSG Sun06Sep2020 {fcG}
    More python goodies

---
+++ script.py
@@ -0,0 +1,10 @@
+#/Users/moonpie/xword/script.py Sun06Sep2020 {fcG}
+
+from datetime import datetime
+import random
+
+current_time = datetime.now()
+
+print(current_time)
+r1 = random.randint(0, 100)  
+print(r1) # Random integer where 0 <= r
